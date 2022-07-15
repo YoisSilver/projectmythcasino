816c301cdc4ad87a0a1151c71b8426818876b704
YoisSilver <azharchd1@gmail.com>
2022-07-15 10:06:29 +0530

    update

--- pages/landing.js
+++ pages/landing.js
@@ -3,12 +3,7 @@ import React from 'react'
 import { useState } from 'react'
 import Image from 'next/image'
 import LandingPage from '../components/LandingPage'
-import Auction from '../components/Auction'
-import Landing from '../components/Landing'
-import Navbar from '../components/Navbar'
-import Toparea from '../components/Toparea'
-import Footer from '../components/Footer'
-import Link from 'next/link'
+
 
 export default function Home() {
 
